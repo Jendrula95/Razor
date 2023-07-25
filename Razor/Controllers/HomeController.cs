f099a27a92d269c26b74db453f4cb4bc1fe19c4b
Jendrula95 <Jendrula95@hotmail.com>
2023-07-25 20:43:02 +0200

    Add project files.

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+using System.Diagnostics;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 100,
+            Name = "Kajak",
+            Description = "Łódka jednoosobowa",
+            Price = 275M
+        };
+
+
+
+
+        public IActionResult Index()
+        {
+            return View(myProduct);
+        }
+       
+        public ActionResult DemoExpression() 
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExspressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+            return View(myProduct);
+        }
+        public ActionResult DemoArray()
+        {
+            Product [] array =
+              {
+                  new Product {Name = "Kajak",Price = 275M },
+                  new Product {Name = "Kamizelka ratunkowa",Price = 48.95M },
+                  new Product {Name = "Piłka nożna",Price = 19.50M },
+                  new Product {Name = "Flaga narożna",Price = 34.95M }
+              };
+            return View(array);
+            
+        }
+    }
+}
---
+++ Razor/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace Razor.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        [DisplayName("Produkt")]
+        public string Name { get;  set; }
+        public string Description { get; set; }
+        [DisplayName("Cena")]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
